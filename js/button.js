3b8ebbebe8507379351fd245174a94a2f9fb6e18
Kan₿a <223156552+0xKanba@users.noreply.github.com>
2026-05-02 12:48:38 +0300

    Update button.js

--- js/button.js
+++ js/button.js
@@ -17,7 +17,7 @@
   style.textContent = `
 #pwaInstallBtn {
   position: fixed;
-  bottom: 21px;          /* فوق الفوتر */
+  bottom: 89px;          /* فوق الفوتر */
   left: 50%;
   transform: translateX(-50%) translateY(20px);
   z-index: 900;
