e02a4faad561bc7a5c1130194181e02923f9b720
Kan₿a <223156552+0xKanba@users.noreply.github.com>
2026-08-09 00:37:28 +0300

    Update index.jsx

--- privy-widget/src/index.jsx
+++ privy-widget/src/index.jsx
@@ -138,7 +138,7 @@ createRoot(mountNode).render(
         name: 'سيولة مبسطة',
         theme: 'dark',
         accentColor: '#00ccff',
-        logo: 'https://cdn.jsdelivr.net/gh/0xKanba/assets@master/hl/images/icon-512x512.png'
+        logo: 'https://cdn.jsdelivr.net/gh/0xKanba/assets@master/hl/images/icon-512x512.png',
       },
     }}
   >
